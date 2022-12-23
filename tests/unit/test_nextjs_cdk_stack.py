3c318555c6d933141cadad153b5078192fe1cf20
David Thomas <david@authenticins.com>
2022-12-22 19:26:42 -0500

    Initial commit

---
+++ tests/unit/test_nextjs_cdk_stack.py
@@ -0,0 +1,15 @@
+import aws_cdk as core
+import aws_cdk.assertions as assertions
+
+from nextjs_cdk.nextjs_cdk_stack import NextjsCdkStack
+
+# example tests. To run these tests, uncomment this file along with the example
+# resource in nextjs_cdk/nextjs_cdk_stack.py
+def test_sqs_queue_created():
+    app = core.App()
+    stack = NextjsCdkStack(app, "nextjs-cdk")
+    template = assertions.Template.from_stack(stack)
+
+#     template.has_resource_properties("AWS::SQS::Queue", {
+#         "VisibilityTimeout": 300
+#     })
